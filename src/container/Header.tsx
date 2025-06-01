98477e1921d7c0e93c39a72fa263719966e8141e
Pawan <pawanpyakurel3000@gmail.com>
2025-06-01 11:56:30 +0545

    simple state handling

--- src/container/Header.tsx
+++ src/container/Header.tsx
@@ -1,13 +1,30 @@
+import { useState } from 'react';
+
 export const Header = () => {
+  const [selected, setSelected] = useState('');
 
-    return (
-        <div className="flex flex-row justify-between items-center">
-            <button className="text-white font-bold">Today</button>
-            <button className="text-white">My List</button>
-            <button className="text-white">News</button>
-            <button className="text-white">Profile</button>
-        </div>
-    )
-}
-
-
+  return (
+    <div className='flex flex-row justify-between items-center'>
+      <button
+        className='text-white font-bold'
+        onClick={() => setSelected('today')}>
+        Today
+      </button>
+      <button
+        className='text-white'
+        onClick={() => setSelected('mylist')}>
+        My List
+      </button>
+      <button
+        className='text-white'
+        onClick={() => setSelected('news')}>
+        News
+      </button>
+      <button
+        className='text-white'
+        onClick={() => setSelected('profile')}>
+        Profile
+      </button>
+    </div>
+  );
+};
