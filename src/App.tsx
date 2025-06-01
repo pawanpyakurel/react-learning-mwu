5b1261d86c53ee6822786b1748c86fe62c4525ea
Pawan <pawan.pyakurel@crackajackllc.com>
2025-06-01 10:53:03 +0545

    project init and tailwind setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Header } from "./container/header"
+
+
+function App() {
+  return (
+    <div >
+      <Header />
+      <h1 className="text-red-500" >Hello World</h1>
+    </div>
+   
+  )
+}
+
+export default App
---
+++ src/container/Header.tsx
@@ -0,0 +1,13 @@
+export const Header = () => {
+
+    return (
+        <div className="flex flex-row justify-between items-center">
+            <button className="text-white font-bold">Today</button>
+            <button className="text-white">My List</button>
+            <button className="text-white">News</button>
+            <button className="text-white">Profile</button>
+        </div>
+    )
+}
+
+
