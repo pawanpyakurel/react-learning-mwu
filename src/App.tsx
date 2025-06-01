01471a3db02a8da50966b43ebb3caf3f7be94526
Pawan <pawanpyakurel3000@gmail.com>
2025-06-01 15:00:00 +0545

    basic api call

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,38 @@
+import { useEffect } from 'react';
 import { HeroCard } from './components/HeroCard';
 import { Header } from './container/Header';
 
 function App() {
+  const getWeather = async () => {
+    try {
+      const res = await fetch(
+        `https://goweather.xyz/v2/weather/surkhet?unit=u`,
+        {
+          method: 'GET',
+        }
+      );
+
+      console.log(res);
+    } catch (err) {
+      console.log(err?.response);
+    }
+  };
+
+  useEffect(() => {
+    getWeather();
+  }, []);
+
   return (
     <div className='flex flex-col justify-center items-center gap-5 p-4'>
       <div className='w-full'>
         <Header />
       </div>
       <HeroCard />
+      <button
+        className='text-white bg-blue-500 p-2 rounded-md'
+        onClick={getWeather}>
+        Get Weather
+      </button>
     </div>
   );
 }
